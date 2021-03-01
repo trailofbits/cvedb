053f0eb4630bcde2da1108ccdac69388dfc0672b
Evan Sultanik <evan.sultanik@trailofbits.com>
2021-03-01 11:23:36 -0500

    Randomly generate CPEs for testing

--- test/test_cpe.py
+++ test/test_cpe.py
@@ -1,8 +1,59 @@
+from random import choice, randint
+from typing import Callable, TypeVar, Union
 from unittest import TestCase
 
-from cvedb.db import CVEdb
-from cvedb.cpe import CPE, Logical, parse_formatted_string, Part
-from cvedb.cve import Configurations
+from rstr import xeger
+
+from cvedb.cpe import (
+    AV_STRING_REGEX, CPE, FormattedStringParser, LANGTAG_REGEX, Language, Logical, parse_formatted_string, Part
+)
+
+
+assert AV_STRING_REGEX.pattern.endswith(".*")
+assert LANGTAG_REGEX.pattern.endswith(".*")
+
+
+def random_avstring() -> str:
+    return xeger(AV_STRING_REGEX.pattern[:-2])
+
+
+def random_language() -> Language:
+    while True:
+        try:
+            return FormattedStringParser(xeger(LANGTAG_REGEX.pattern[:-2])).parse_langtag()
+        except ValueError:
+            pass
+
+
+T = TypeVar("T")
+
+
+def random_logical_or(gen: Callable[[], T] = random_avstring) -> Union[Logical, T]:
+    r = randint(0, 2)
+    if r == 0:
+        return Logical.ANY
+    elif r == 1:
+        return Logical.NA
+    while True:
+        ret = gen()
+        if ret != Logical.ANY.value and ret != Logical.NA.value:
+            return ret
+
+
+def random_cpe() -> CPE:
+    return CPE(
+        part=random_logical_or(lambda: choice(list(Part))),
+        vendor=random_logical_or(),
+        product=random_logical_or(),
+        version=random_logical_or(),
+        update=random_logical_or(),
+        edition=random_logical_or(),
+        lang=random_logical_or(random_language),
+        sw_edition=random_logical_or(),
+        target_sw=random_logical_or(),
+        target_hw=random_logical_or(),
+        other=random_logical_or()
+    )
 
 
 class TestCPE(TestCase):
@@ -21,11 +72,9 @@ class TestCPE(TestCase):
         self.assertEqual(cpe.other, Logical.ANY)
 
     def test_serialization(self):
-        with CVEdb.open() as db:
-            for i, cve in enumerate(db.data()):
-                if i > 100:
-                    break
-                self.assertEqual(cve.configurations, Configurations.loads(cve.configurations.dumps()))
+        for i in range(100):
+            cpe = random_cpe()
+            self.assertEqual(cpe, CPE.loads(cpe.dumps()))
 
     def test_wildcards(self):
         self.assertTrue(CPE().is_complete_wildcard())
