ba28d937842de8e6463674dd682f417957ecdd99
Evan Sultanik <evan.sultanik@trailofbits.com>
2021-05-11 11:22:14 -0400

    Only require `dataclasses` for Python<3.7 (#6)

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     install_requires=[
         "cvss>=2.2",
         # dataclasses were added in Python 3.7, so use this backport for earlier versions of Python
-        "dataclasses",
+        "dataclasses;python_version<'3.7'",
         "python-dateutil>=2.8.1",
         "tqdm>=4.48.0"
     ],
